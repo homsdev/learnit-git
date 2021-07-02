cd211ac5dc9a72448d70621e7adbcab098b6bfe4
Omar Briseño <omar.briseno@ironbit.com.mx>
2021-07-02 12:56:21 -0500

    Implemented UserGreeting Class in HelloWorld Class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World From Java!!!");
+		UserGreeting msg= new UserGreeting();
+		msg.executeSayHello();
 	}
 
 }
